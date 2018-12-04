e40fc70902ca993f5bc6ccb16d942de75f37eec8
MaDyY <MaDyY@Mangesh>
2018-12-04 14:41:36 +0530

    commit first

---
+++ pravin/src/pravin/Test.java
@@ -0,0 +1,32 @@
+package pravin;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int i;
+		for(i=0;i<=100;i++) 
+		{
+			 if (i%3==0 && i%5==0) 
+			{
+				System.out.println("pravinmaddy123");
+			}
+			 else if(i%3==0) 
+			{
+				System.out.println("maddy");
+			}
+			
+			else if(i%5==0) 
+			{
+				System.out.println("pravin");
+			}
+			
+			
+			
+		}
+		
+	}
+
+}
